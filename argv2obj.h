b887812c79b34f3fc9ac1b0cb29e6b09bb97dfdc
Daniel Patru <dpatru@gmail.com>
2014-10-01 22:01:30 -0400

    Dynamically set parameters with Abstract class.

--- argv2obj.h
+++ argv2obj.h
@@ -6,9 +6,17 @@
 #include <stdio.h>  /* printf */
 #include <cstdlib>  /* atoi, exit */
 #include <string>   /* string */
+#include <sstream>   /* stringstream */
 #include <cctype>   /* tolower */
 #include <typeinfo> /* (typeid result).name == "bool" */
+
+#include <vector>
+#include <map>
+
 using std::string;
+using std::vector;
+using std::map;
+using std::stringstream;
 
     
 int min (int i, int j) { return i < j? i: j; };
@@ -76,105 +84,45 @@ char* tolower(char* s) {
   return s;
 };
 
-
-class flag_t {
- public:
-  bool value;
-  char *help;     // help text
-  char *shorts;   // short arguments
-  char **longv;   // long arguments
-  int longc;  // number of long arguments
-
-  flag_t ( ) {};
-  flag_t ( char *_shorts, char** _longv, int _longc ) {
-    init(_shorts, _longv, _longc);
-  };
-  
-  void init ( char *_shorts, char** _longv, int _longc ) {
-    this->shorts = _shorts;
-    this->longv = _longv;
-    this->longc = _longc;
-  };
-  
-  /// Parse argv starting at argv[i][j]. Return true on error. Update
-  /// i and j as needed.
-  bool parse ( int &i, int &j, const int argc, const char **argv ) {
-    // i = index into argv, j = index into argv[i]
-    if ( i >= argc) return false; // no more arguments
-    if ( j > 0 && j >= strlen ( argv[i] )) { 
-      i++; j = 0; // move to next argument if at the end argv[i]
-      if ( i >= argc) return false; // return if no more arguments
-    }
-    if ( '-' == argv[i][0] ) {
-      if ('-' == argv[i][1]) {
-	// -- means stop processing
-	if ('\0' == argv[i][2]) return false;
-	j=2;
-	return this->parselong ( i, j, argc, argv );
-      }
-      if (j < 1) j = 1;
-      printf("parsing short i=%d j=%d %c\n", i, j, argv[i][j]);
-      return parseshort ( i, j, argc, argv );
-    }
-    return false; // not long or short option, just return
-  };
-
-  bool parselong ( int &i, int &j, const int argc, const char **argv ) {
-    // we start pointing at the long
-    // printf("checking %s, parsing long,  %s %s\n", this->longv[0], argv[i]+j, argv[i]);
-    // printf("i = %d, j = %d\n", i, j);
-    
-    for ( int _i = 0; _i < this->longc; _i++ ) {
-      // printf("checking %s, parsing long,  %s %s\n", this->longv[_i], argv[i]+j, argv[i]);
-      char *l = this->longv[_i];
-      if ( startswith ( l, argv[i]+2 ) ) {
-	// printf("startswith %s %s\n", this->longv[_i], argv[i]+2);
-	char char_after = argv[i][ 2 + strlen(l) ];
-	switch ( char_after ) {
-	case '\0': // value in next argument
-	  this->value = true;
-	  i++; j = 0;
-	  return false;
-	case '=': // value in this argument
-	  atoval ( argv[i]+2+strlen(l)+1, this->value );
-	  i += 1; j = 0;
-	  return false;
-	default: ; // is longer than what we're looking for
-	}
-      }
-    }
-    return false;
-  };
-  
-  bool parseshort ( int &i, int &j, const int argc, const char **argv ) {
-    // check shorts
-    printf("flag parseshort\n");
-    if (j < 1) { printf("parse error, j <= 0\n"); exit(1); }
-    for ( int _i = strlen(this->shorts)-1; _i >= 0; _i-- ) {
-      if ( this->shorts[_i] == argv[i][j] ) {
-	this->value = true;
-	j++;
-	return false;
-      }
-    }
-    return false;
-  };
-
+struct abstractParameter_t {
+  //  virtual ~abstractParameter_t() = 0;
+  virtual bool parse ( int &i, int &j, const int argc, const char **argv ) = 0;
+  virtual const char* c_str() = 0;
+  ~abstractParameter_t(){};
 };
 
-template < class T>
-class parameter_t {
+template < class T >
+struct parameter_t: public abstractParameter_t {
  public:
-  T value;
-  char *help;     // help text
-  char *shorts;   // short arguments
-  char **longv;   // long arguments
-  int longc;  // number of long arguments
+  parameter_t () {};
+  parameter_t ( char _short, char* _long ) {
+    init ( _short, _long );
+  };
+  parameter_t ( char _short, char* _long, T def ) {
+    init ( _short, _long, def );
+  };
+  
+  // virtual ~parameter_t(){};
+  ~parameter_t(){};
 
-  void init ( char *_shorts, char** _longv, int _longc ) {
-    this->shorts = _shorts;
-    this->longv = _longv;
-    this->longc = _longc;
+  T value;
+  string _str;
+  const char *c_str() {
+    stringstream s; s << value; this->_str = s.str(); return this->_str.c_str();
+  };
+  
+  char *help;     // help text
+  char s;   // short argument
+  char *l;   // long argument
+
+  void init ( char _short, char* _long, T def) {
+    this->init ( _short, _long);
+    this->value = def;
+  };
+  
+  void init ( char _short, char* _long ) {
+    this->s = _short;
+    this->l = _long;
   };
   
   /// Parse argv starting at argv[i][j]. Return true on error. Update
@@ -205,35 +153,30 @@ class parameter_t {
     // printf("checking %s, parsing long,  %s %s\n", this->longv[0], argv[i]+j, argv[i]);
     // printf("i = %d, j = %d\n", i, j);
     
-    for ( int _i = 0; _i < this->longc; _i++ ) {
-      // printf("checking %s, parsing long,  %s %s\n", this->longv[_i], argv[i]+j, argv[i]);
-      char *l = this->longv[_i];
-      if ( startswith ( l, argv[i]+2 ) ) {
-	// printf("startswith %s %s\n", this->longv[_i], argv[i]+2);
-	char char_after = argv[i][ 2 + strlen(l) ];
-	switch ( char_after ) {
-	case '\0': // value in next argument
-	  // printf("type = %s\n", typeid(T).name());
-	  if (! strncmp(typeid(T).name(), "bool", 1)) {
-	    atoval("true", this->value);
-	    i++; j = 0;
-	  }
-	  else if ( i+1 >= argc ) {
-	    printf("Missing argument for option %s\n", argv[i]);
-	    i++; j=0;// avoid infinite loop
-	    return true;
-	  }
-	  else {
-	    atoval (argv[i+1], this->value);
-	    i += 2; j = 0;
-	  }
-	  return false;
-	case '=': // value in this argument
-	  atoval ( argv[i]+2+strlen(l)+1, this->value );
-	  i += 1; j = 0;
-	  return false;
-	default: ; // is longer than what we're looking for
+    if ( startswith ( this->l, argv[i]+2 ) ) {
+      char char_after = argv[i][ 2 + strlen(l) ];
+      switch ( char_after ) {
+      case '\0': // value in next argument
+	// printf("type = %s\n", typeid(T).name());
+	if (! strncmp(typeid(T).name(), "bool", 1)) {
+	  atoval("true", this->value);
+	  i++; j = 0;
 	}
+	else if ( i+1 >= argc ) {
+	  printf("Missing argument for option %s\n", argv[i]);
+	  i++; j=0;// avoid infinite loop
+	  return true;
+	}
+	else {
+	  atoval (argv[i+1], this->value);
+	  i += 2; j = 0;
+	}
+	return false;
+      case '=': // value in this argument
+	atoval ( argv[i]+2+strlen(l)+1, this->value );
+	i += 1; j = 0;
+	return false;
+      default: ; // is longer than what we're looking for
       }
     }
     return false;
@@ -242,90 +185,65 @@ class parameter_t {
   bool parseshort ( int &i, int &j, const int argc, const char **argv ) {
     // check shorts
     if (j < 1) { printf("parse error, j <= 0\n"); exit(1); }
-    for ( int _i = strlen(this->shorts)-1; _i >= 0; _i-- ) {
-      // printf("type = %s\n", typeid(T).name());
-      if ( this->shorts[_i] == argv[i][j] ) {
-	// printf("%c matches %c, in %s, 2 over: %s\n", argv[i][j], this->shorts[_i], argv[i], argv[i]+2);
-	if (! strncmp(typeid(T).name(), "bool", 1)) {
-	  atoval("true", this->value);
-	  j++;
-	}
-	else if (j+1 < strlen(argv[i])) {
-	  atoval ( argv[i]+j+1, this->value );
-	  i++; j = 0;
-	}
-	else if ( i + 1 < argc ) {
-	  atoval ( argv[i+1], this->value );
-	  i += 2; j=0;
-	}
-	else {
-	  printf ("missing value for short option %c in %s\n", this->shorts[_i], argv[i]);
-	  i++; j=0; // avoid infinite loop if return value is not checked.
-	  return true;
-	}
-	return false;
+    // printf("type = %s\n", typeid(T).name());
+    if ( this->s == argv[i][j] ) {
+      if (! strncmp(typeid(T).name(), "bool", 1)) {
+	atoval("true", this->value);
+	j++;
       }
+      else if (j+1 < strlen(argv[i])) {
+	atoval ( argv[i]+j+1, this->value );
+	i++; j = 0;
+      }
+      else if ( i + 1 < argc ) {
+	atoval ( argv[i+1], this->value );
+	i += 2; j=0;
+      }
+      else {
+	printf ("missing value for short option %c in %s\n", this->s, argv[i]);
+	i++; j=0; // avoid infinite loop if return value is not checked.
+	return true;
+      }
+      return false;
     }
     return false;
   };
 
-  parameter_t () {};
-  parameter_t ( char *_shorts, char** _longv, int _longc ) {
-    init(_shorts, _longv, _longc);
-  };
-  
 };
 
+
+typedef map < string, abstractParameter_t* > pmap_t;
 
 class parameters_t {
  public:
 
-  //  flag_t myBool;
-  parameter_t < bool > myBool;
-  parameter_t < int > myInt;
-  parameter_t < double > myFloat;
-  parameter_t < string > myString;
-  parameter_t < char* > myChars;
-  
-  
-  /// Return true if flag matches. Set variable as side effect.
   parameters_t () {};
   parameters_t ( int argc, const char ** argv ) { this->init(argc, argv); };
+  pmap_t p; 
   void init(int argc, const char **argv) {
     
-    char *cs[] = {(char*) "bool", (char*) "my-bool"};
-    this->myBool.init((char*) "b", cs, 2);
-    
-    char *cs2[] = {(char*) "int", (char*) "my-int"};
-    this->myInt.init((char*) "i", cs2, 2);
+    p["myBool"] = new parameter_t < bool > ('b', (char*) "bool", false); 
+    p["myInt"] = new parameter_t < int > ('i', (char*) "int", 0); 
+    p["myFloat"] = new parameter_t < double > ('f', (char*) "float", 0.0); 
+    p["myString"] = new parameter_t < string > ('S', (char*) "string", ""); 
+    p["myChars"] = new parameter_t < char* > ('s', (char*) "chars", (char *) ""); 
 
-    char *cs3[] = {(char*) "float", (char*) "my-float"};
-    this->myFloat.init((char*) "f", cs3, 2);
+    // p["myChars"] = &myChars;
     
-    char *cs4[] = {(char*) "string", (char*) "my-string"};
-    this->myString.init((char*) "S", cs4, 2);
-    
-    char *cs5[] = {(char*) "chars", (char*) "my-chars"};
-    this->myChars.init((char*) "s", cs5, 2);
-  
     int i=1, j=0;
     int oldi, oldj;
     do {
       oldi = i; oldj = j;
-      myBool.parse ( i, j, argc, argv );
-      myInt.parse ( i, j, argc, argv );
-      myFloat.parse ( i, j, argc, argv );
-      myString.parse ( i, j, argc, argv );
-      myChars.parse ( i, j, argc, argv );
+      for ( pmap_t::iterator it = p.begin(); it != p.end(); it++) {
+	it->second->parse ( i, j, argc, argv );
+      }
     } while ( oldi != i || oldj != j );
 
     // check 
     printf ( "\nparameters are: \n" );
-    printf ( " bool = %d\n", this->myBool.value );
-    printf ( " int = %d\n", this->myInt.value );
-    printf ( " float = %f\n", this->myFloat.value );
-    printf ( " string = %s\n", this->myString.value.c_str() );
-    printf ( " chars = %s\n", this->myChars.value );
+    for ( pmap_t::iterator it = p.begin(); it != p.end(); it++) {
+      printf( "%s=%s\n", it->first.c_str(), it->second->c_str());
+    }
     printf ( "\n\n" );
   };
 
--- test.cpp
+++ test.cpp
@@ -1,5 +1,32 @@
 #include "argv2obj.h"
 
+#include <vector>
+
+struct AbstractField{
+  virtual ~AbstractField() = 0;
+};
+
+template<class T,int fieldTypeId>
+class Field: public AbstractField{
+  private:
+    T field;
+  public:
+    const static int field_type;
+  public:
+    virtual ~Field(){}
+};
+
+class Database_Record{
+  std::vector<AbstractField*> record; 
+  public:
+    ~Database_Record(){
+      //delete all AbstractFields in vector
+    }
+};
+
+
+
+
 int main ( int argc, const char** argv ) {
   parameters_t args ( argc, argv );
 }
